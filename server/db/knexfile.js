6d24ff89460877cd80a5b54b921228de700b8a42
Callum Morris <callummorris@Cal.local>
2021-11-29 11:58:49 +1300

    config for heroku

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -17,7 +17,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL,
       ssl: { rejectUnauthorized: false }
